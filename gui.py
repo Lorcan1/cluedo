428546aee05c3cc3b95e97d49f9f1f9b5eae742f
Lorcan1 <lorcan.rooney@ucdconnect.ie>
2021-07-08 10:09:00 +0100

    added logo at start

--- gui.py
+++ gui.py
@@ -2,39 +2,34 @@ from tkinter import *
 from PIL import ImageTk,Image
 import player
 import gameplay,gamesetup,board
+import time
 
 height = 700
 width = 700
 squareSize = height // 25
-# root= Tk()
+boardImg = ImageTk.PhotoImage(Image.open("board.jpg"))
 
 class Gui:
     def __init__(self):
-        # # self.root = Tk()
-        # self.root.mainloop()
         self.createBoard()
     def createBoard(self):
         board.b.board.pack(anchor = 'nw')
-        img = ImageTk.PhotoImage(Image.open("board.jpg"))
-        board.b.board.create_image(20,20,anchor=NW,image=img)
-   #     (h1,h2,w1,w2) = player.p.placePlayersOnBoard()
-        player.p.placePlayersOnBoard()
-   #     board.b.board.create_rectangle(w1, h1, w2, h2, fill=p.colour)
-        # i want gameplay to happen here 
+        logoImg = Image.open("cluedo.png")
+        resize_image = logoImg.resize((500,500))
+        logoImg = ImageTk.PhotoImage(resize_image)  
+        board.b.board.create_image(width/2, height/2, image=logoImg) 
+        board.b.root.after(2000, self.change_img)
         gameplay.gp.startPosition(player.p.allPlayersDict)
         gameplay.gp.rollDice(gamesetup.gs.playersDict)
         print(gameplay.gp.board)
-        #self.root.mainloop()
-        root.mainloop()
+        board.b.root.mainloop()
 
-    # def placePlayersOnBoard(self):
-    #     for p in player.p.allPlayersDict.values():
-    #         print(p.x ,p.y)
-    #         h1,h2 = 44 + 22.92*(p.x),44 + 22.92*(p.x +1)
-    #         w1,w2 = 63 + 22.91*(p.y),63 + 22.91*(p.y +1)
-    #         self.board.create_rectangle(w1, h1, w2, h2, fill=p.colour)
-        #self.board.create_rectangle(63, 204.44, 613, 227.36, fill='black')
-        # self.board.create_rectangle(240, 594.08, 250, 617, fill='black')
+
+    def change_img(self):
+        board.b.board.delete("all")
+        board.b.board.create_image(20,20, anchor=NW, image=boardImg)
+        player.p.placePlayersOnBoard()
+
 gui = Gui()
 # gui.createBoard()
 
